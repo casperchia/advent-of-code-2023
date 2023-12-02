787bead70db2d34ce431adf6694e5f1f68e7066f
Casper Chia <casper.chia@dws.com.au>
2023-12-02 12:42:57 +1100

    Disable unnecessary Jest warnings

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.(ts|tsx)?$": [
+      "ts-jest",
+      { diagnostics: { ignoreCodes: ["TS151001"] } },
+    ],
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
 };
