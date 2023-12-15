a7dea30485a97c2d2ea18dad7deada5d5336b07f
Casper Chia <casper.chia@dws.com.au>
2023-12-15 20:59:48 +1100

    Day 10 part 2 solution

--- src/day10/day10b.ts
+++ src/day10/day10b.ts
@@ -1,3 +1,63 @@
+import {
+  Direction,
+  getNextDirection,
+  getNextPoint,
+  getStartingPoint,
+  Grid,
+  isPointInGrid,
+  Legend,
+  parseInput,
+  Point,
+} from "./day10a";
+
+export const getPathCoordinates = (
+  grid: Grid,
+  [x, y]: Point,
+  direction: Direction,
+): Point[] => {
+  const points: Point[] = [];
+
+  while (true) {
+    const nextDirection = getNextDirection(direction, grid[y][x]);
+    if (!nextDirection) break;
+
+    const [nextX, nextY] = getNextPoint([x, y], nextDirection);
+    if (!isPointInGrid([nextX, nextY], grid)) break;
+
+    x = nextX;
+    y = nextY;
+    direction = nextDirection;
+    points.push([x, y]);
+
+    if (grid[y][x] === "S") return points;
+  }
+
+  return [];
+};
+
+const isPointingNorth = (legend: Legend): boolean =>
+  ["|", "J", "L"].includes(legend);
+
 export const solve = () => {
-  return null;
+  const grid = parseInput("src/day10/day10.input.txt");
+  const startingPoint = getStartingPoint(grid);
+  let coordinates = getPathCoordinates(grid, startingPoint, "Up");
+  if (coordinates.length === 0)
+    coordinates = getPathCoordinates(grid, startingPoint, "Down");
+  if (coordinates.length === 0)
+    coordinates = getPathCoordinates(grid, startingPoint, "Left");
+
+  let tiles = 0;
+  let isInside = false;
+  grid.forEach((row, y) => {
+    grid[y].forEach((legend, x) => {
+      const isPartOfLoop = coordinates.find(([aX, aY]) => aX === x && aY === y);
+      if (isPartOfLoop && isPointingNorth(legend)) {
+        isInside = !isInside;
+      } else if (!isPartOfLoop && isInside) {
+        tiles++;
+      }
+    });
+  });
+  return tiles;
 };
